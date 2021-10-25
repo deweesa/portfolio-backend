d4a43ab47947b6d39da3f97f41c485d2eb7c1caf
deweesa <asadeweese@gmail.com>
2021-10-25 16:32:48 -0700

    Added mongoose to the project

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,8 @@ const express = require("express");
 const blogRoutes = express.Router();
 const dbo = require("../db/conn");
 
+let Post = require('../models/post.model');
+
 const ObjectId = require("mongodb").ObjectId;
 
 blogRoutes.route('/blog').get((req, res) => {
@@ -14,5 +16,19 @@ blogRoutes.route('/blog').get((req, res) => {
             res.json(result);
         });
 })
+
+blogRoutes.route('/blog/makerand').get((req,res) => {
+    const title = 'pingus chunus'
+    const content = 'this is a real pingus chungus right here, no joke'
+
+    const newPost = new Post({
+        title,
+        content
+    })
+
+    newPost.save()
+    .then(() => res.json('Post added'))
+    .catch(err => res.status(400).json('error: ' + err));
+});
 
 module.exports = blogRoutes;
--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const mongoose = require('mongoose');
 
 require('dotenv').config({ path: './config.env' })
 const port = process.env.port || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDb database connection established successfully");
+})
+
 app.use(require("./routes/record"));
 app.use(require("./routes/blog"));
 
 const dbo = require("./db/conn")
 
 app.listen(port, () => {
-    dbo.connectToServer(function (err) {
-        if (err) console.error(err);
-    });
     console.log(`Server is running on port: ${port}`);
-});
+})
